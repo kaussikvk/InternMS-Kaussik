563fd8e48652ef0bc2b5f398ed9ae53b93b622e6
Kaussik <kaussikvk@gmail.com>
2026-07-23 13:21:54 +0530

    feat: Add GitHub Actions workflow for deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/internmslp/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
